273e9bfeb643ee5f63d6722215513e7f10217c6c
mansur4321 <jet_011@mail.ru>
2022-09-18 17:41:22 +0300

    setup
    Создал приложение. Протестировал, что tailwind работает

---
+++ form-city/src/App.vue
@@ -0,0 +1,20 @@
+<script lang="js">
+import headerC from './components/header-c.vue'
+
+export default {
+  name: 'App',
+  components: {
+    headerC,
+  }
+}
+</script>
+
+<template>
+  <div class="h-100px w-full">
+  <p>првиет мир</p>
+  <header-c></header-c>
+  </div>
+</template>
+
+<style>
+</style>
---
+++ form-city/src/components/header-c.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full flex pt-20px">
+    <p>aaaaaaa</p>
+    <p>bbbbb</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
